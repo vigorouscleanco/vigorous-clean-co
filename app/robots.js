fe395e8495069c1297021a8a3c4b61b234360800
vigorouscleanco <vigorouscleanco@gmail.com>
2025-09-27 18:16:11 -0700

    Create robots.js

---
+++ app/robots.js
@@ -0,0 +1,10 @@
+// app/robots.js
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://vigorous-clean-co.vercel.app/sitemap.xml",
+  };
+}
