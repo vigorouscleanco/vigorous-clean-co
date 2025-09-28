bf4d62f461e34864bba567c06cd1b450c4c299a8
vigorouscleanco <vigorouscleanco@gmail.com>
2025-09-28 09:54:37 -0700

    Create page.js

---
+++ app/login/page.js
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div className="container" style={{padding:"28px 24px"}}>
+      <h1>Login</h1>
+      <p>Account features coming soon.</p>
+    </div>
+  );
+}
