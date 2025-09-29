67e52cda86c6efb1bc13f385307d92d38a2d74b7
vigorouscleanco <vigorouscleanco@gmail.com>
2025-09-28 21:42:26 -0700

    Update and rename page.js to page.jsx

--- app/signup/page.js
+++
@@ -1,9 +0,0 @@
-export default function SignupPage() {
-  return (
-    <main style={{maxWidth:600,margin:"40px auto",padding:"0 20px",textAlign:"center"}}>
-      <h1>Create Account</h1>
-      <p>Sign-up coming soon. You can still book right now.</p>
-      <a href="https://book.squareup.com/appointments/0d8cas6ix7qhc2/location/L9K9470312P89/services" className="btn btn-solid" style={{display:"inline-block",marginTop:16}}>Book Now</a>
-    </main>
-  );
-}
