dc82851b88ca6455d7a12d82791ee3197ed0488c
vigorouscleanco <vigorouscleanco@gmail.com>
2025-09-28 21:02:35 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+content: [
+"./app/**/*.{js,jsx,ts,tsx,mdx}",
+"./pages/**/*.{js,jsx,ts,tsx,mdx}",
+"./components/**/*.{js,jsx,ts,tsx,mdx}",
+],
+theme: { extend: {} },
+plugins: [],
 };
