4d034c724dc37513b13de1c277450bdf3a8fae56
vigorouscleanco <vigorouscleanco@gmail.com>
2025-09-28 21:40:37 -0700

    Update and rename page.js to page.jsx

--- app/privacy/page.js
+++
@@ -1,8 +0,0 @@
-export default function PrivacyPage() {
-  return (
-    <main style={{maxWidth:800,margin:"40px auto",padding:"0 20px"}}>
-      <h1>Privacy Policy</h1>
-      <p>We collect basic info to schedule and complete services. We don’t sell personal data. Questions? Email vigorouscleanco@gmail.com.</p>
-    </main>
-  );
-}
